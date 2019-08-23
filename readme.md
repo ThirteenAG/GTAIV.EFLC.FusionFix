f4e450a7ee899f78c3ce6782002eb821f30056f2
ThirteenAG <thirteenag@gmail.com>
2019-08-23 23:59:44 +0300

    added ini, readme

--- readme.md
+++ readme.md
@@ -1,3 +1,93 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/wwokiviriq3m52t6?svg=true)](https://ci.appveyor.com/project/ThirteenAG/gtaiv-eflc-fusionfix)
 
+<p align="center">
+  <img height="100" src="https://user-images.githubusercontent.com/4904157/63623173-921aaf00-c601-11e9-8b84-fc5803269323.png">
+</p>
+
 # GTAIV.EFLC.FusionFix
+
+This projects aims to fix some issues in Grand Theft Auto IV and Episodes from Liberty City.
+
+Changelog:
+
+- [Fixed recoil behavior that was different when playing with keyboard/mouse and gamepad](https://github.com/GTAmodding/GTAIV-Issues-List/issues/6)
+- [Fixed forced "definition off" setting in cutscenes, now cutscenes will respect the menu setting](https://github.com/GTAmodding/GTAIV-Issues-List/issues/5)
+- [Partially fixed emissive shader bug, namely BAWSAQ building is still rendered incorrectly, but the rest of the lights are now present in the game (e.g. Rotterdam Tower, Comrades Bar area)](https://github.com/GTAmodding/GTAIV-Issues-List/issues/4)
+- [Fixed DLC car lights in TBoGT](https://github.com/GTAmodding/GTAIV-Issues-List/issues/3)
+- [Tree foliage casts dynamic shadows instead of static](https://github.com/GTAmodding/GTAIV-Issues-List/issues/9)
+
+## Installation:
+
+#### GTAIV 1.0.8.0 / GTAIV 1.0.8.0 + DLC Episodes / GTA EFLC 1.0.3.0
+
+This fix is tested only with latest official update and [XLiveLessAddon](https://bitbucket.org/ThirteenAG/xlivelessaddon/downloads/).
+
+The following installation instructions assume that you're either have it already, or willing to install it.
+Online features of the game are not available with [XLiveLessAddon](https://bitbucket.org/ThirteenAG/xlivelessaddon/downloads/).
+
+---
+
+If you only want the core package, that includes all aforementioned fixes,
+
+### **Download**: [GTAIV.EFLC.FusionFix.Min.zip](https://github.com/ThirteenAG/GTAIV.EFLC.FusionFix/releases/latest/download/GTAIV.EFLC.FusionFix.Min.zip)
+
+This requires ASI Loader.
+
+---
+
+If you want complete package for easy installation, that includes all aforementioned fixes and [Ultimate ASI Loader](https://github.com/ThirteenAG/Ultimate-ASI-Loader) as **XLiveLess**, plus [XLiveLessAddon](https://bitbucket.org/ThirteenAG/xlivelessaddon/downloads/) to make the game work without xlive,
+
+### **Download**: [GTAIV.EFLC.FusionFix.Mid.zip](https://github.com/ThirteenAG/GTAIV.EFLC.FusionFix/releases/latest/download/GTAIV.EFLC.FusionFix.Mid.zip)
+
+---
+
+If you want to install other mods along the way, including all aforementioned fixes, [Ultimate ASI Loader](https://github.com/ThirteenAG/Ultimate-ASI-Loader) as **XLiveLess**, plus [XLiveLessAddon](https://bitbucket.org/ThirteenAG/xlivelessaddon/downloads/) to make the game work without xlive, [ColAccel](https://github.com/ThirteenAG/IV.EFLC.ColAccel), a plugin that reduces loading times, [Project2DFX](https://github.com/ThirteenAG/III.VC.SA.IV.Project2DFX), a plugin that adds lod lights,
+
+### **Download**: [GTAIV.EFLC.FusionFix.Max.zip](https://github.com/ThirteenAG/GTAIV.EFLC.FusionFix/releases/latest/download/GTAIV.EFLC.FusionFix.Max.zip)
+
+---
+
+Unpack the content of archive to your **GTAIV** or **EFLC** directory.
+
+**TBoGT as DLC note**: if you have episodes installed as DLC in your **GTAIV** folder, you need to move
+
+**Grand Theft Auto IV/TBoGT/pc/models/cdimages/vehicles.img**
+
+to
+
+**Grand Theft Auto IV/DLC/TBoGT/content/pc/models/cdimages/vehicles.img**
+
+---
+
+## Files rundown:
+
+    TBoGT/pc/models/cdimages/vehicles.img
+
+**Car lights fix for TBoGT.**
+
+    pc/data/maps/props/vegetation/ext_veg.ide
+    pc/data/maps/props/vegetation/ext_veg.img
+
+**Trees shadows fix**
+
+    common/shaders/win32_30/gta_emissivestrong.fxc
+    common/shaders/win32_30_atidx10/gta_emissivestrong.fxc
+    common/shaders/win32_30_low_ati/gta_emissivestrong.fxc
+    common/shaders/win32_30_nv6/gta_emissivestrong.fxc
+    common/shaders/win32_30_nv7/gta_emissivestrong.fxc
+    common/shaders/win32_30_nv8/gta_emissivestrong.fxc
+
+**Corrected shaders to make the lights appear. Used in conjunction with GTAIV.EFLC.FusionFix.asi**
+
+    plugins/GTAIV.EFLC.FusionFix.asi
+    plugins/GTAIV.EFLC.FusionFix.ini
+
+**Recoil, definition and shader bugs are fixed here.**
+
+# Reporting more issues
+
+We're making a list of **GTA IV** issues that weren't addressed in official patches.
+
+If you can think of one, submit it [here](https://github.com/GTAmodding/GTAIV-Issues-List/issues).
+
+If you've encountered an issue, caused by this fix, report it [here](https://github.com/ThirteenAG/GTAIV.EFLC.FusionFix/issues).
--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -2,48 +2,61 @@
 
 void Init()
 {
-    //recoil fix
-    static float fRecMult = 0.65f;
-    auto pattern = hook::pattern("F3 0F 10 44 24 ? F3 0F 59 05 ? ? ? ? EB ? E8");
-    injector::WriteMemory(pattern.get_first(10), &fRecMult, true);
+    CIniReader iniReader("");
+    bool bRecoilFix = iniReader.ReadInteger("MAIN", "RecoilFix", 1) != 0;
+    bool bDefinitionFix = iniReader.ReadInteger("MAIN", "DefinitionFix", 1) != 0;
+    bool bEmissiveShaderFix = iniReader.ReadInteger("MAIN", "EmissiveShaderFix", 1) != 0;
 
-    //disable forced definition-off in cutscenes
-    pattern = hook::pattern("E8 ? ? ? ? F6 D8 1A C0 F6 D0 22 05 ? ? ? ? C3"); //0x88CC6B
-    injector::WriteMemory<uint16_t>(pattern.get_first(11), 0xA090, true); //mov al ...
-
-    // workaround for gta_emissivestrong.fxc lights on patch 6+,
-    //"A0 01 00 00 02 00 00 08" replaced in shader files with "A1 01 00 00 02 00 00 08" (5 occurrences)
-    pattern = hook::pattern("C1 E7 04 C1 E3 04 8B C7 83 F8 04 8D 9B ? ? ? ? 8B CB 72 14 8B 32 3B 31 75 12 83 E8 04");
-    struct ShaderTest
+    if (bRecoilFix)
     {
-        void operator()(injector::reg_pack& regs)
+        static float fRecMult = 0.65f;
+        auto pattern = hook::pattern("F3 0F 10 44 24 ? F3 0F 59 05 ? ? ? ? EB ? E8");
+        injector::WriteMemory(pattern.get_first(10), &fRecMult, true);
+    }
+
+    if (bDefinitionFix)
+    {
+        //disable forced definition-off in cutscenes
+        auto pattern = hook::pattern("E8 ? ? ? ? F6 D8 1A C0 F6 D0 22 05 ? ? ? ? C3"); //0x88CC6B
+        injector::WriteMemory<uint16_t>(pattern.get_first(11), 0xA090, true); //mov al ...
+    }
+
+    if (bEmissiveShaderFix)
+    {
+        // workaround for gta_emissivestrong.fxc lights on patch 6+,
+        //"A0 01 00 00 02 00 00 08" replaced in shader files with "A1 01 00 00 02 00 00 08" (5 occurrences)
+        auto pattern = hook::pattern("C1 E7 04 C1 E3 04 8B C7 83 F8 04 8D 9B ? ? ? ? 8B CB 72 14 8B 32 3B 31 75 12 83 E8 04");
+        struct ShaderTest
         {
-            if (
-                *(uint32_t*)(regs.edx + 0x00) == 0x39D1B717 &&
-                *(uint32_t*)(regs.edx + 0x10) == 0x41000000 && //traffic lights and lamps
-                *(uint32_t*)(regs.edx - 0x10) == 0x3B03126F &&
-                *(uint32_t*)(regs.edx + 0x20) == 0x3E59999A &&
-                *(uint32_t*)(regs.edx + 0x24) == 0x3F372474 &&
-                *(uint32_t*)(regs.edx + 0x28) == 0x3D93A92A
-            )
+            void operator()(injector::reg_pack& regs)
             {
-                *(float*)(regs.edx + 0x00) = -*(float*)(regs.edx + 0x00);
+                if (
+                    *(uint32_t*)(regs.edx + 0x00) == 0x39D1B717 &&
+                    *(uint32_t*)(regs.edx + 0x10) == 0x41000000 && //traffic lights and lamps
+                    *(uint32_t*)(regs.edx - 0x10) == 0x3B03126F &&
+                    *(uint32_t*)(regs.edx + 0x20) == 0x3E59999A &&
+                    *(uint32_t*)(regs.edx + 0x24) == 0x3F372474 &&
+                    *(uint32_t*)(regs.edx + 0x28) == 0x3D93A92A
+                )
+                {
+                    *(float*)(regs.edx + 0x00) = -*(float*)(regs.edx + 0x00);
+                }
+
+                regs.edi = regs.edi << 4;
+                regs.ebx = regs.ebx << 4;
             }
+        }; injector::MakeInline<ShaderTest>(pattern.count(2).get(1).get<void>(0), pattern.count(2).get(1).get<void>(6)); //417800
 
-            regs.edi = regs.edi << 4;
-            regs.ebx = regs.ebx << 4;
-        }
-    }; injector::MakeInline<ShaderTest>(pattern.count(2).get(1).get<void>(0), pattern.count(2).get(1).get<void>(6)); //417800
-
-    //pattern = hook::pattern("0F B6 29 0F B6 32 2B F5 75 45 83 E8 01 83 C1 01 83 C2 01 85 C0");
-    //struct ShaderTest2
-    //{
-    //    void operator()(injector::reg_pack& regs)
-    //    {
-    //        *(uint8_t*)&regs.ebp = *(uint8_t*)(regs.ecx + 0x00);
-    //        *(uint8_t*)&regs.esi = *(uint8_t*)(regs.edx + 0x00);
-    //    }
-    //}; injector::MakeInline<ShaderTest2>(pattern.count(6).get(3).get<void>(0), pattern.count(6).get(3).get<void>(6)); //41782D
+        //pattern = hook::pattern("0F B6 29 0F B6 32 2B F5 75 45 83 E8 01 83 C1 01 83 C2 01 85 C0");
+        //struct ShaderTest2
+        //{
+        //    void operator()(injector::reg_pack& regs)
+        //    {
+        //        *(uint8_t*)&regs.ebp = *(uint8_t*)(regs.ecx + 0x00);
+        //        *(uint8_t*)&regs.esi = *(uint8_t*)(regs.edx + 0x00);
+        //    }
+        //}; injector::MakeInline<ShaderTest2>(pattern.count(6).get(3).get<void>(0), pattern.count(6).get(3).get<void>(6)); //41782D
+    }
 
     //draw distance adjuster
     //static float f_dist_mult = 10.0f;
