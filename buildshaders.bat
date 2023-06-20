a1e7fc3de38d5b82dd17993abe461ad37a9fab58
ThirteenAG <thirteenag@gmail.com>
2023-06-20 14:43:44 +0300

    fix appveyor

--- buildshaders.bat
+++ buildshaders.bat
@@ -10,38 +10,38 @@ for /R "%shaders_path%" %%a in (*.xml) do (
 )
 cd ../..
 
-FOR /F "delims=" %%i IN ('where /r shaders\\GTA-IV-Shaders mirror.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_cutout_fence.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection deferred_lighting.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_cubemap_reflect.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_cutout_fence.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_default.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_emissive.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_emissivenight.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_emissivestrong.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_cubemap_reflect.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_reflect.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_spec.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_spec_cubemap_reflect.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_spec_reflect.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_parallax.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_parallax_specmap.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_parallax_steep.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_reflect.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_rmptfx_gpurender.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_rmptfx_raindrops.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_spec.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_spec_reflect.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_trees.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_vehicle_paint1.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_vehicle_paint2.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_vehicle_paint3.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection rage_billboard_nobump.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection rage_default.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection rage_postfx.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection water.fxc') DO call :copyShader "%%i"
-FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection waterTex.fxc') DO call :copyShader "%%i"
+FOR /F "delims=" %%i IN ('where /r shaders\\GTA-IV-Shaders mirror.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_cutout_fence.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection deferred_lighting.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_cubemap_reflect.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_cutout_fence.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_default.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_emissive.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_emissivenight.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_emissivestrong.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_cubemap_reflect.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_reflect.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_spec.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_spec_cubemap_reflect.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_normal_spec_reflect.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_parallax.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_parallax_specmap.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_parallax_steep.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_reflect.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_rmptfx_gpurender.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_rmptfx_raindrops.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_spec.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_spec_reflect.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_trees.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_vehicle_paint1.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_vehicle_paint2.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection gta_vehicle_paint3.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection rage_billboard_nobump.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection rage_default.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection rage_postfx.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection water.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
+FOR /F "delims=" %%i IN ('where /r shaders\\GTAIV.ShaderFixesCollection waterTex.fxc') DO ( echo D | xcopy "%%i" "%win32_30%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_atidx10%" /K /H /Y && echo D | xcopy "%%i" "%win32_30_nv8%" /K /H /Y )
 goto :eof
 
 :copyShader
