b6b350799f4be6abdbff73f5b0e88076a62cc3da
Tomasak <Tomasak@users.noreply.github.com>
2023-07-20 15:39:48 +0200

    Update ini and img building

--- buildimg.bat
+++ buildimg.bat
@@ -1,3 +1,6 @@
+md .\data\update\GTAIV.EFLC.FusionFix\IV
+md .\data\update\GTAIV.EFLC.FusionFix\TLAD
+md .\data\update\GTAIV.EFLC.FusionFix\TBOGT
 cd tools
 cd IMGConsole
 fastman92ImgConsole32.exe -script "..\..\data\update\GTAIV.EFLC.FusionFix\cdimagemake.ims"
