6f14d3a7fb0151569710284842a685cdff4bc588
ThirteenAG <thirteenag@gmail.com>
2023-12-28 13:49:52 +0800

    ci for textures

---
+++ buildwtd.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal EnableDelayedExpansion
+set src=%cd%\textures\
+set dst=%cd%\data\update\
+
+cd tools
+cd ResourceBuilder
+for /f "delims=" %%i in ('dir %src% /a:d/s/b') do (
+  set "foldername=%%i"
+  if /I "!foldername:~-4!"==".wtd" (
+    set "string=%%~dpi"
+    set "modified=!string:%src%=%dst%!"
+    if not exist !modified! mkdir !modified!
+    set "string=%%i"
+    set "modified=!string:%src%=%dst%!"
+    ResourceBuilder.exe -c_wtd_v8 !modified! -f %%i
+  )
+)
+cd ..
+cd ..
